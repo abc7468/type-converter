0e34bedb9ceb8c180c0ffcc0ff2358112a74d03d
SeongHyun <abc7468@naver.com>
2022-04-24 04:00:52 +0900

    fix : 환경 수정

--- WebService/kafka/producer.go
+++ WebService/kafka/producer.go
@@ -49,37 +49,3 @@ func APIDataProducer(cfg kafka.ConfigMap) *kafka.Producer {
 	}
 	return p
 }
-
-// func Producer1() {
-
-// 	p := FormatDataProducer()
-// 	defer p.Close()
-
-// 	// Delivery report handler for produced messages
-// 	go func() {
-// 		for e := range p.Events() {
-// 			switch ev := e.(type) {
-// 			case *kafka.Message:
-// 				if ev.TopicPartition.Error != nil {
-// 					fmt.Printf("Delivery failed: %v\n", ev.TopicPartition)
-// 				} else {
-// 					fmt.Printf("Delivered message to %v\n", ev.TopicPartition)
-// 				}
-// 			}
-// 		}
-// 	}()
-
-// 	// Produce messages to topic (asynchronously)
-// topic := "myTopic"
-
-// for _, word := range []string{"Welcome", "to", "the", "Confluent", "Kafka", "Golang", "client"} {
-// 	_ = p.Produce(&kafka.Message{
-// 		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
-// 		Value:          []byte(word),
-// 	}, nil)
-// }
-
-// 	// Wait for message deliveries before shutting down
-// 	p.Flush(15 * 1000)
-
-// }
